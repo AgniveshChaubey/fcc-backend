07e7a359476b429bfa8957490a192b116f0d93f3
Agnivesh Chaubey <agniveshvapi@gmail.com>
2023-02-13 01:02:11 +0530

    learned how to serve html file and static assets

--- 2nd/myApp.js
+++ 2nd/myApp.js
@@ -1,10 +1,19 @@
 let express = require('express');
 let app = express();
 
+console.log("Hello World");
+
+// app.get('/', (req, res)=>{
+//     res.send("Hello Express");
+// })
 
 
+app.get('/', (req, res) =>{
+    res.sendFile(__dirname+"/views/index.html");
+})
 
 
+app.use('/public', express.static(__dirname+"/public"))
 
 
 
