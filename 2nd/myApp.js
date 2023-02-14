2b0ad7b2dabac4572f1b9ca521e8d69602661fed
Agnivesh Chaubey <agniveshvapi@gmail.com>
2023-02-14 20:50:21 +0530

    learned how to use .env file to store variables

--- 2nd/myApp.js
+++ 2nd/myApp.js
@@ -1,48 +1,29 @@
 let express = require('express');
 let app = express();
 
-console.log("Hello World");
+// console.log("Hello World");
 
 // app.get('/', (req, res)=>{
 //     res.send("Hello Express");
 // })
 
 
-app.get('/', (req, res) =>{
-    res.sendFile(__dirname+"/views/index.html");
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + "/views/index.html");
 })
 
+app.use('/public', express.static(__dirname + "/public"))
 
-app.use('/public', express.static(__dirname+"/public"))
+// app.get('/json', (req, res)=>{
+//     res.send({"message": "Hello json"})
+// })
 
+app.get('/json', (req, res) => {
+    if (process.env.MESSAGE_STYLE == "uppercase") {
+        res.json({ "message": "Hello json".toUpperCase() })
+    } else {
+        res.json({ "message": "Hello json" })
+    }
+})
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
